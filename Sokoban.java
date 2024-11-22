4fd5883e4349fb6ea363aee96efda9ad2ac67492
guzzang <991012kim@gmail.com>
2024-11-22 15:41:44 +0900

    소코반 1단계 중 데이터 저장 단계 완료

--- Sokoban.java
+++ Sokoban.java
@@ -1,7 +1,19 @@
 public class Sokoban {
 
+    static class SokobanMap{
+        int numEmptySpace;
+        int numHall;
+        int numBall;
+        int numPlayer;
+        int numWall;
+        int stageSeparator;
+        String originalMap;
+    }
+
+    static SokobanMap map = new SokobanMap();
+
     public static void main(String[] args) {
-        String sokobanMap = """
+        String mapInformation = """
                 Stage 1
                 #####
                 #OoP#
@@ -16,14 +28,48 @@ public class Sokoban {
                  #   O  #\s
                  ########
                 """;
-        inputSokobanMap(sokobanMap);
+        inputSokobanMapInformation(mapInformation);
+        saveNumberToSokobanMapInformation(mapInformation);
 
+        System.out.println("Halls: " + map.numHall);
+        System.out.println("Balls: " + map.numBall);
+        System.out.println("Players: " + map.numPlayer);
+        System.out.println("Walls: " + map.numWall);
+        System.out.println("Empty Spaces: " + map.numEmptySpace);
     }
 
-    // 해당 내용을 문자열로 받아서 출력
-    static void inputSokobanMap(String sokobanMap) {
-        System.out.println(sokobanMap);
+    // map을 문자열로 받고 SokobanMap class에 map 정보를 저장
+    static void inputSokobanMapInformation(String mapInformation) {
+        map.originalMap = mapInformation;
     }
 
+    // map의 각 구성요소에 대해 대응되는 저장값으로 변환
+    static Integer saveSokobanMapInformationToNumber(char mapInformation) {
 
+        if(mapInformation == ' ') return 0;
+        else if(mapInformation == 'O') return 1;
+        else if(mapInformation == 'o') return 2;
+        else if(mapInformation == 'P') return 3;
+        else if(mapInformation == '#') return 4;
+
+        return null;
+    }
+
+    // map을 순회하며 구성요소의 해당 저장값에 대응하는 SokobanMap class의 변수값 증가
+    static void saveNumberToSokobanMapInformation(String mapInformation){
+        String[] mapRowInformation = mapInformation.split("\n");
+        for(String mapRow :mapRowInformation) {
+            for(int i = 0; i < mapRow.length(); i++) {
+                if(saveSokobanMapInformationToNumber(mapRow.charAt(i)) != null){
+                    switch (saveSokobanMapInformationToNumber(mapRow.charAt(i))) {
+                        case 1 -> map.numHall++;
+                        case 2 -> map.numBall++;
+                        case 3 -> map.numPlayer++;
+                        case 4 -> map.numWall++;
+                        case 0 -> map.numEmptySpace++;
+                    }
+                }
+            }
+        }
+    }
 }

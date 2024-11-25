51512c3d85c7f4a1cb021a76af84a2a13b446534
guzzang <991012kim@gmail.com>
2024-11-25 18:03:31 +0900

    소코반 1단계 중 출력 단계 리팩토링 완료

--- Sokoban.java
+++ Sokoban.java
@@ -1,89 +1,73 @@
 public class Sokoban {
 
     static class SokobanMap{
+        String stageNumber;
         int numEmptySpace;
-        int numHall;
-        int numBall;
-        int numPlayer;
-        int numWall;
-        int stageSeparator;
-        String originalMap;
-    }
-
-    static class SokobanMapStage1{
         int mapWidth;
         int mapHeight;
         int numHall;
         int numBall;
         int[] playerPosition = new int[2];
-        String originalMapStage1;
+        String originalMapStage;
     }
 
-    static class SokobanMapStage2{
-        int mapWidth;
-        int mapHeight;
-        int numHall;
-        int numBall;
-        int[] playerPosition = new int[2];
-        String originalMapStage2;
-    }
 
-    static SokobanMap map = new SokobanMap();
-    static SokobanMapStage1 map1 = new SokobanMapStage1();
-    static SokobanMapStage2 map2 = new SokobanMapStage2();
-
+    static SokobanMap map1 = new SokobanMap();
+    static SokobanMap map2 = new SokobanMap();
     static String[] mapArray;
 
     public static void main(String[] args) {
-        String mapInformation = """
+        String mapInformation =
+                """
                 Stage 1
                 #####
                 #OoP#
                 #####
                 =====
                 Stage 2
-                  #######
+                  #######                    
                 ###  O  ###
                 #    o    #
                 # Oo P oO #
                 ###  o  ###
-                 #   O  #
-                 ########
+                 #   O  #  
+                 ########  
                 """;
-        saveMapStageInformation(mapInformation);
-        // inputSokobanMapInformation(mapInformation);
-        saveNumberToSokobanMapInformation(map1.originalMapStage1);
-        saveNumberToSokobanMapInformation(map2.originalMapStage2);
+        splitMapStages(mapInformation);
+        calculateMapInformation(map1);
+        calculateMapInformation(map2);
 
-        System.out.println("map1.originalMapStage1 = " + map1.originalMapStage1);
-        System.out.println("map1.mapWidth = " + map1.mapWidth);
-        System.out.println("map1.mapHeight = " + map1.mapHeight);
-        System.out.println("Map1 Halls: " + map1.numHall);
-        System.out.println("Map1 Balls: " + map1.numBall);
-        System.out.println("map1.playerPosition[0] = " + map1.playerPosition[0]);
-        System.out.println("map1.playerPosition[1] = " + map1.playerPosition[1]);
+        printMap(map1);
+        printMap(map2);
 
-        System.out.println("map2.originalMapStage2 = " + map2.originalMapStage2);
-        System.out.println("map2.mapWidth = " + map2.mapWidth);
-        System.out.println("map2.mapHeight = " + map2.mapHeight);
-        System.out.println("Map2 Halls: " + map2.numHall);
-        System.out.println("Map2 Balls: " + map2.numBall);
-        System.out.println("map2.playerPosition[0] = " + map2.playerPosition[0]);
-        System.out.println("map2.playerPosition[1] = " + map2.playerPosition[1]);
     }
 
-    static void saveMapStageInformation(String mapInformation) {
+    static void splitMapStages(String mapInformation){
         mapArray = mapInformation.split("=====");
-        map1.originalMapStage1 = mapArray[0];
-        map2.originalMapStage2 = mapArray[1];
+        saveStageDetails(map1, mapArray[0].trim());
+        saveStageDetails(map2, mapArray[1].trim());
     }
 
-    // map을 문자열로 받고 SokobanMap class에 map 정보를 저장
-    static void inputSokobanMapInformation(String mapInformation) {
-        map.originalMap = mapInformation;
+    static void saveStageDetails(SokobanMap map, String mapInformation) {
+        String[] splitMapInformation = mapInformation.split("\n", 2);
+        map.stageNumber = splitMapInformation[0];
+        map.originalMapStage = splitMapInformation[1];
+
     }
 
-    // map의 각 구성요소에 대해 대응되는 저장값으로 변환
+    static void calculateMapInformation(SokobanMap map) {
+        String[] mapRowInformation = map.originalMapStage.split("\n");
+        calculateMapWidthAndHeight(map, mapRowInformation);
+        countMapInformation(map, mapRowInformation);
+    }
+
+    static void calculateMapWidthAndHeight(SokobanMap map, String[] mapRowInformation) {
+        map.mapHeight = mapRowInformation.length;
+        for (String mapRow : mapRowInformation) {
+            map.mapWidth = Math.max(map.mapWidth, mapRow.length());
+        }
+    }
+
     static Integer saveSokobanMapInformationToNumber(char mapInformation) {
 
         if(mapInformation == ' ') return 0;
@@ -95,49 +79,75 @@ public class Sokoban {
         return null;
     }
 
-    // map을 순회하며 구성요소의 해당 저장값에 대응하는 SokobanMap class의 변수값 증가
-    static void saveNumberToSokobanMapInformation(String mapInformation){
-        int max = Integer.MIN_VALUE;
-        if(mapInformation.startsWith("Stage 1")){
-            String[] mapRowInformation = mapInformation.split("\n");
-            for(int i = 0; i < mapRowInformation.length; i++) {
-                map1.mapWidth = Math.max(max, mapRowInformation[i].length());
-                map1.mapHeight++;
-                for(int j = 0; j <mapRowInformation[i].length(); j++) {
-                    if(saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j)) != null){
-                        switch (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j))){
-                            case 1 -> map1.numHall++;
-                            case 2 -> map1.numBall++;
-                            case 3 -> {
-                                map1.playerPosition[0] = i;
-                                map1.playerPosition[1] = j + 1;
-                            }
+    // map의 각 구성요소에 대해 대응되는 저장값으로 변환
+    static void countMapInformation(SokobanMap map, String[] mapRowInformation) {
+        for (int i = 0; i < mapRowInformation.length; i++) {
+            for (int j = 0; j < mapRowInformation[i].length(); j++) {
+                if (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j)) != null) {
+                    switch (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j))) {
+                        case 0 -> map.numEmptySpace++;
+                        case 1 -> map.numHall++;
+                        case 2 -> map.numBall++;
+                        case 3 -> {
+                            map.playerPosition[0] = i + 1;
+                            map.playerPosition[1] = j + 1;
                         }
                     }
                 }
             }
-            map1.mapHeight -= 1;
-        }
-        else{
-            String[] mapRowInformation = mapInformation.split("\n");
-            for(int i = 0; i < mapRowInformation.length; i++) {
-                map2.mapWidth = Math.max(max, mapRowInformation[i].length());
-                map2.mapHeight++;
-                for(int j = 0; j <mapRowInformation[i].length(); j++) {
-                    if(saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j)) != null){
-                        switch (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j))){
-                            case 1 -> map2.numHall++;
-                            case 2 -> map2.numBall++;
-                            case 3 -> {
-                                map2.playerPosition[0] = i - 1;
-                                map2.playerPosition[1] = j + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            map2.mapHeight -= 2;
         }
     }
+
+    static void printMap(SokobanMap map) {
+        System.out.println(map.stageNumber);
+        System.out.println("\n");
+        System.out.println(map.originalMapStage);
+        System.out.println("\n");
+        System.out.println("가로크기: " + map.mapWidth);
+        System.out.println("세로크기: " + map.mapHeight);
+        System.out.println("구멍의 수: " + map.numHall);
+        System.out.println("공의 수: " + map.numBall);
+        System.out.println("플레이어 위치: " + map.playerPosition[0] + "행 " + map.playerPosition[1] + "열");
+        System.out.println("\n");
+    }
+
+    // map을 순회하며 구성요소의 해당 저장값에 대응하는 SokobanMap class의 변수값 증가
+//    static void saveNumberToSokobanMapInformation(SokobanMap map, String mapInformation){
+//        if(map.stageNumber.equals("Stage 1")){
+//            String[] mapRowInformation = mapInformation.split("\n");
+//            for(int i = 0; i < mapRowInformation.length; i++) {
+//                map1.mapWidth = Math.max(map1.mapWidth, mapRowInformation[i].length());
+//                for(int j = 0; j <mapRowInformation[i].length(); j++) {
+//                    if(saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j)) != null){
+//                        switch (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j))){
+//                            case 0 -> map1.numEmptySpace++;
+//                            case 1 -> map1.numHall++;
+//                            case 2 -> map1.numBall++;
+//                            case 3 -> {
+//                                map1.playerPosition[0] = i + 1;
+//                                map1.playerPosition[1] = j + 1;}}}}}
+//            map1.mapHeight = mapRowInformation.length;
+//        }
+//        else{
+//            String[] mapRowInformation = mapInformation.split("\n");
+//            for(int i = 0; i < mapRowInformation.length; i++) {
+//                map2.mapWidth = Math.max(map2.mapWidth, mapRowInformation[i].length());
+//                for(int j = 0; j < mapRowInformation[i].length(); j++) {
+//                    if(saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j)) != null){
+//                        switch (saveSokobanMapInformationToNumber(mapRowInformation[i].charAt(j))){
+//                            case 0 -> map2.numEmptySpace++;
+//                            case 1 -> map2.numHall++;
+//                            case 2 -> map2.numBall++;
+//                            case 3 -> {
+//                                map2.playerPosition[0] = i + 1;
+//                                map2.playerPosition[1] = j + 1;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            map2.mapHeight = mapRowInformation.length;
+//        }
+//    }
 
 }
